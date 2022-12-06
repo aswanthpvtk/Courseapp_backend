cf69f7bae4bf75669ba075680486672ff6a189b2
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-06 11:11:06 +0530

    course

--- src/main/java/com/nest/courseapp_backend/controller/coursecontroller.java
+++ src/main/java/com/nest/courseapp_backend/controller/coursecontroller.java
@@ -1,6 +1,7 @@
 package com.nest.courseapp_backend.controller;
 
 import com.nest.courseapp_backend.model.Course;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +20,11 @@ public class coursecontroller {
     {
         System.out.println(s.getTitle().toString());
         return "adding the book";
+    }
+
+    @GetMapping("/view")
+    public String Viewall()
+    {
+        return "view all books";
     }
 }
